0c4732df232db372cd4dcb8e336675a4e11c6103
stephHents <stephanierazaka1@gmail.com>
2024-06-11 09:31:03 +0300

    sprint5-ETU2632

--- src/java/FrontController.java
+++ src/java/FrontController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import map.*;
+import exception.*;
 import utils.*;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,10 +11,11 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.*;
-import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import annotation.Annotation_Get;
-import utils.ControllerScanner;
 
 public class FrontController extends HttpServlet {
     private String namePackage;
@@ -27,22 +29,42 @@ public class FrontController extends HttpServlet {
             super.init(configurer);
             ServletContext context = configurer.getServletContext();
             namePackage = context.getInitParameter("package-controller");
+            if (namePackage == null || namePackage.isEmpty()) {
+                throw new BuildException("Le package des contrôleurs n'est pas spécifié ou est vide.");
+            }
             this.scanner = new ControllerScanner();
             this.controllers = this.scanner.findControllers(namePackage);
-
-            for (Class<?> controllerClass : controllers) {
-                Method[] methods = controllerClass.getDeclaredMethods();
-                for (Method method : methods) {
-                    if (method.isAnnotationPresent(Annotation_Get.class)) {
-                        Annotation_Get annotation = method.getAnnotation(Annotation_Get.class);
-                        String url = annotation.value();
-                        Mapping mapping = new Mapping(controllerClass.getName(), method.getName());
-                        urlMapping.put(url, mapping);
-                    }
-                }
+            if (this.controllers.isEmpty()) {
+                throw new BuildException("Aucun contrôleur trouvé dans le package spécifié : " + namePackage);
             }
+
+            mapControllers();
+        } catch (BuildException | RequestException e) {
+            throw new ServletException(e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void mapControllers() throws RequestException {
+        for (Class<?> controllerClass : controllers) {
+            Method[] methods = controllerClass.getDeclaredMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(Annotation_Get.class)) {
+                    Annotation_Get annotation = method.getAnnotation(Annotation_Get.class);
+                    String url = annotation.value();
+                    if (urlMapping.containsKey(url)) {
+                        throw new RequestException("Duplication d'URL détectée pour : " + url);
+                    }
+                    // Vérifier le type de retour
+                    Class<?> returnType = method.getReturnType();
+                    if (!returnType.equals(String.class) && !returnType.equals(ModelView.class)) {
+                        throw new RequestException("La méthode " + method.getName() + " dans " + controllerClass.getName() + " a un type de retour invalide : " + returnType.getName());
+                    }
+                    Mapping mapping = new Mapping(controllerClass.getName(), method.getName());
+                    urlMapping.put(url, mapping);
+                }
+            }
         }
     }
 
@@ -67,50 +89,54 @@ public class FrontController extends HttpServlet {
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-    try {
-        PrintWriter out = response.getWriter();
-        String fullUrl = request.getRequestURI();
-        String url = extractUrl(fullUrl);
+            throws ServletException, IOException {
+        try {
+            PrintWriter out = response.getWriter();
+            String fullUrl = request.getRequestURI();
+            String url = extractUrl(fullUrl);
 
-        out.println("<h1>Bienvenue</h1>");
-        out.println("<h3>Lien: " + url + "</h3>");
+            out.println("<h1>Bienvenue</h1>");
+            out.println("<h3>Lien: " + url + "</h3>");
 
-        Mapping mapping = urlMapping.get(url);
+            Mapping mapping = urlMapping.get(url);
 
-        if (mapping != null) {
-            out.println("<h3>URL: " + url + " - Mapping: " + mapping.getClassName() + "#" + mapping.getMethodName() + "</h3>");
+            if (mapping != null) {
+                out.println("<h3>URL: " + url + " - Mapping: " + mapping.getClassName() + "#" + mapping.getMethodName() + "</h3>");
 
-            Class<?> controllerClass = Class.forName(mapping.getClassName());
+                Class<?> controllerClass = Class.forName(mapping.getClassName());
 
-            Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
+                Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
 
-            Method method = controllerClass.getMethod(mapping.getMethodName());
+                Method method = controllerClass.getMethod(mapping.getMethodName());
 
-            Object result = method.invoke(controllerInstance);
+                Object result = method.invoke(controllerInstance);
 
-            if (result instanceof String) {
-                out.println("<h3>Résultat: " + result + "</h3>");
-            } else if (result instanceof ModelView) {
-                ModelView modelView = (ModelView) result;
-                String viewUrl = modelView.getUrl();
-                HashMap<String, Object> data = modelView.getData();
+                // Vérifier le type de retour
+                if (result instanceof String) {
+                    out.println("<h3>Résultat: " + result + "</h3>");
+                } else if (result instanceof ModelView) {
+                    ModelView modelView = (ModelView) result;
+                    String viewUrl = modelView.getUrl();
+                    HashMap<String, Object> data = modelView.getData();
 
-                for (String key : data.keySet()) {
-                    request.setAttribute(key, data.get(key));
+                    for (String key : data.keySet()) {
+                        request.setAttribute(key, data.get(key));
+                    }
+
+                    RequestDispatcher dispatcher = request.getRequestDispatcher(viewUrl);
+                    dispatcher.forward(request, response);
+                } else {
+                    throw new RequestException("Type de résultat non reconnu pour l'URL : " + url);
                 }
-
-                RequestDispatcher dispatcher = request.getRequestDispatcher(viewUrl);
-                dispatcher.forward(request, response);
             } else {
-                out.println("<h3>Type de résultat non reconnu</h3>");
+                throw new RequestException("URL non trouvée : " + url);
             }
-        } else {
-            out.println("<h3>Aucune méthode associée à ce chemin</h3>");
+        } catch (RequestException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Une erreur interne s'est produite.");
         }
-    } catch (Exception e) {
-        e.printStackTrace();
     }
-}
-
 }
--- src/java/Mapping.java
+++ src/java/Mapping.java
@@ -1,27 +1,46 @@
 package map;
 
+import exception.RequestException;
+import utils.*;
+
+import java.lang.reflect.Method;
+
 public class Mapping {
     String className;
     String methodName;
-    
+
     public Mapping(String className, String methodName) {
         this.className = className;
         this.methodName = methodName;
     }
-    
+
     public String getClassName() {
         return className;
     }
-    
+
     public String getMethodName() {
         return methodName;
     }
-    
+
     public void setClassName(String className) {
         this.className = className;
     }
-    
+
     public void setMethodName(String methodName) {
         this.methodName = methodName;
+    }
+
+    public void reflectMethod() throws RequestException {
+        try {
+            Class<?> clazz = Class.forName(className);
+            Method method = clazz.getMethod(methodName);
+            Class<?> returnType = method.getReturnType();
+
+            if (!returnType.equals(String.class) && !returnType.equals(ModelView.class)) {
+                throw new RequestException("La méthode " + methodName + " dans " + className + " doit retourner un String ou un ModelView");
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            throw new RequestException("Erreur lors de la réflexion sur la méthode : " + e.getMessage());
+        }
     }
 }
